f8898bc48d6ab804fd01a6f1ea7af17f65b5c359
lewiskk016 <lewiskk016@gmail.com>
2023-03-20 17:12:00 -0400

    Stopped at my_flatten

--- enumerables_array.rb
+++ enumerables_array.rb
@@ -6,11 +6,11 @@ class Array
     def my_each(&blk)
         i = 0
         while i< self.length
-           
+
             blk.call(self[i])
             i += 1
         end
-        self 
+        self
     end
 
 
@@ -27,10 +27,51 @@ class Array
         my_each {|ele| result << ele if blk.call(ele) == true}
         result
     end
-    a = [1, 2, 3] # my_each = a.each
-   p a.my_select { |num| num > 1 } # => [2, 3]
-   p a.my_select { |num| num == 4 } # => []
+#     a = [1, 2, 3] # my_each = a.each
+#    p a.my_select { |num| num > 1 } # => [2, 3]
+#    p a.my_select { |num| num == 4 } # => []
+
+    def my_reject(&blk)
+        result = []
+        my_each {|ele| result << ele if blk.call(ele) == false}
+        result
+    end
+
+    # a = [1, 2, 3]
+    # p a.my_reject { |num| num > 1 } # => [1]
+    # p a.my_reject { |num| num == 4 } # => [1, 2, 3]
+
+    def my_any?(&blk)
+        my_each do |ele|
+            return true if blk.call(ele) == true
+        end
+        false
+    end
+
+    def my_all?(&blk)
+        my_each do |ele|
+            return false if blk.call(ele) == false
+        end
+        true
+    end
+
+    # a = [1, 2, 3]
+    # p a.my_any? { |num| num > 1 } # => true
+    # p a.my_any? { |num| num == 4 } # => false
+
+    # p a.my_all? { |num| num > 1 } # => false
+    # p a.my_all? { |num| num < 4 } # => true
+
+    def my_flatten
+        return self if !self.is_a?(Array)
+        result = []
+        my_each { |ele| result += my_flatten(ele) }
+        result
+    end
+    p [1, 2, 3, [4, [5, 6]], [[[7]], 8]].my_flatten
+
+    # def my_zip(*n)
+
+    # end
 
 end
-
-
